fb2cf36dd6d21872cb5747df6d4c08921f34287d
wes <103287237+notWeso@users.noreply.github.com>
2022-06-03 16:06:00 -0700

    Delete p1-date.js

--- p1-date.js
+++
@@ -1,9 +0,0 @@
-/*
-    CIT 281 Project 1
-    Name: Wes O'Brien
-*/
-
-var today = new Date();
-  var day = today.getDay();
-  var daylist = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
-  console.log("the current day is " + daylist[day] + ".");
