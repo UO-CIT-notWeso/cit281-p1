a296996d1efa2c48618516bdd7359f11f60e15e4
wes <103287237+notWeso@users.noreply.github.com>
2022-06-03 16:07:18 -0700

    Add files via upload
    adding project 1 files and images

---
+++ p1-date.js
@@ -0,0 +1,9 @@
+/*
+    CIT 281 Project 1
+    Name: Wes O'Brien
+*/
+
+var today = new Date();
+  var day = today.getDay();
+  var daylist = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+  console.log("the current day is " + daylist[day] + ".");
